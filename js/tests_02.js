7140576de8385f9630c4ce619fe4e8693501458e
Alex <41620816+PhazZzyo@users.noreply.github.com>
2021-12-12 00:30:15 +0200

    save

--- js/tests_02.js
+++ js/tests_02.js
@@ -1,238 +1,238 @@
-// "use strict";
+// // "use strict";
 
-// -------------- --------- ---- ====***** ( Exercise ) #32
+// // -------------- --------- ---- ====***** ( Exercise ) #32
 
-function includes(array, value) {
-  // Change code below this line
-for (let i = 0; i <= array.length; i += 1) {
-    if (array[i] === value) {
-      return true;
-    }  
-  }
-  return false;
-  
-  // Change code above this line
-}
-
-// -------------- --------- ---- ====***** ( Exercise ) #31
-
-function findNumber(start, end, divisor) {
-  // Change code below this line
-  let number;
-
-  for (let i = start; i < end; i += 1) {
-    if (i % divisor === 0) {
-      return i;      
-    }
-  } 
-  // Change code above this line
-}
-
-
-// -------------- --------- ---- ====***** ( Exercise ) #30
-
-const start = 6;
-const end = 27;
-let number;
-
-for (let i = start; i < end; i += 1) {
-  if (i % 5 === 0) {
-    number = i;
-    break;
-  }
-}
-
-// -------------- --------- ---- ====***** ( Exercise ) #29
-
-function getEvenNumbers(start, end) {
-   // Change code below this line
-
-const evenNumbers = [];
-let array = [];
- for (let i = start; i <= end; i += 1) { // Change this line
-    array.push(i);
-  }  
- for (const number of array) {
-
-    if (number % 2 === 0) {
-      evenNumbers.push(number);
-    }
- }
- 
-  return evenNumbers;
-
-  // function getEvenNumbers(start, end) {
+// function includes(array, value) {
 //   // Change code below this line
-//   const evenNumbers = [];
-//   for (let i = start; i <= end; i += 1) {
-//     if (i % 2 === 0) {
-//       evenNumbers.push(i);
-//     }
+// for (let i = 0; i <= array.length; i += 1) {
+//     if (array[i] === value) {
+//       return true;
+//     }  
 //   }
-//   return evenNumbers;
+//   return false;
+  
+//   // Change code above this line
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #31
+
+// function findNumber(start, end, divisor) {
+//   // Change code below this line
+//   let number;
+
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       return i;      
+//     }
+//   } 
 //   // Change code above this line
 // }
 
 
-    // Change code above this line
-  }
+// // -------------- --------- ---- ====***** ( Exercise ) #30
 
-// -------------- --------- ---- ====***** ( Exercise ) #27
+// const start = 6;
+// const end = 27;
+// let number;
 
-function filterArray(numbers, value) {
-  // Change code below this line
-  const filteredNumbers = [];
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
 
-  for (const number of numbers) {
+// // -------------- --------- ---- ====***** ( Exercise ) #29
 
-    if (number > value) {
-      filteredNumbers.push(number);
-    }
-  }  
+// function getEvenNumbers(start, end) {
+//    // Change code below this line
+
+// const evenNumbers = [];
+// let array = [];
+//  for (let i = start; i <= end; i += 1) { // Change this line
+//     array.push(i);
+//   }  
+//  for (const number of array) {
+
+//     if (number % 2 === 0) {
+//       evenNumbers.push(number);
+//     }
+//  }
+ 
+//   return evenNumbers;
+
+//   // function getEvenNumbers(start, end) {
+// //   // Change code below this line
+// //   const evenNumbers = [];
+// //   for (let i = start; i <= end; i += 1) {
+// //     if (i % 2 === 0) {
+// //       evenNumbers.push(i);
+// //     }
+// //   }
+// //   return evenNumbers;
+// //   // Change code above this line
+// // }
+
+
+//     // Change code above this line
+//   }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #27
+
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+
+//   for (const number of numbers) {
+
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }  
   
-  return filteredNumbers;
-  // Change code above this line
-}
+//   return filteredNumbers;
+//   // Change code above this line
+// }
 
-// -------------- --------- ---- ====***** ( Exercise ) #26
+// // -------------- --------- ---- ====***** ( Exercise ) #26
 
-function calculateTotalPrice(order) {
-  let total = 0;
-  // Change code below this line
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
 
    
-  for (const price of order) {
-  total += price;
-}
-
-  // Change code above this line
-  return total;
-}
-
-
-// -------------- --------- ---- ====***** ( Exercise ) #25
-
-function getCommonElements(array1, array2) {
-  // Change code below this line
-  
-   const array3 = [];
-
-for (const element of array1) {
-    if (array2.includes(element)) {     
-      array3.push(element);
-  }
-  }
-  return array3;
- // Change code above this line
-}
-
-// console.log(getCommonElements([1, 2, 3], [2, 4]));
-
-
-// -------------- --------- ---- ====***** ( Exercise ) #23
-function filterArray(numbers, value) {
-   // Change code below this line
-  
-  const aboveValue = [];
-
-  // for (let i = 0; numbers[i] > value; i += 1) {
-  for (let number of numbers) {
-    if (number > value) {     
-    aboveValue.push(number);
-      continue;
-      }
-  }
-
-  return aboveValue;
-
-  // Change code above this line
-}
-
-
-// -------------- --------- ---- ====***** ( Exercise ) #22
-
-function createArrayOfNumbers(min, max) {
-  const numbers = [];
-  // Change code below this line
-
-  for (let i = min; i <= max; i += 1) { // Change this line
-    numbers.push(i);
-  }
-  
-  // Change code above this line
-  return numbers;
-}
-
-// -------------- --------- ---- ====***** ( Exercise ) #21
-
-function findLongestWord(string) {
-  // Change code below this line
-
-  const stringSplit = string.split(" ");
-let longestWord = "";
-  
-  for (const word of stringSplit) {
-     if (word.length > longestWord.length) {
-  
-  
-longestWord = word;
-     }
-  }
-  
-return longestWord;
-  // Change code above this line
-}
-
-// -------------- --------- ----====***** ( -------------- --------- ----====***** ( Exercise )  )  #20
-
-function calculateTotalPrice(order) {
-  let total = 0;
-  // Change code below this line
-
-  for (let i = 0; i < order.length; i += 1) { // Change this line
-    total += order[i];
-  
-  }
-  
-  // Change code above this line
-  return total;
-}
-
-
-// -------------- --------- ---- ====***** ( Exercise ) #19
-
-// const fruits = ['apple', 'plum', 'pear', 'orange'];
-
-// for (let i = 0;  i < fruits.length; i += 1) { // Change this line
-//   const fruit = fruits[i]; // Change this line
-//   console.log(fruit);
+//   for (const price of order) {
+//   total += price;
 // }
 
-// -------------- --------- ---- ====***** ( Exercise ) #18
-
-function calculateTotal(number) {
- // Change code below this line
-
- let sum = 0;
-
-  for (let i = 1; i <= number; i += 1) {
-    sum += i;
-  }
-
-  return sum;
-
-  // Change code above this line
-}
-
-
-
-
-// -------------- --------- ---- ====***** ( Exercise ) #17
-
-
-// const start = 3;
-// const end = 7;
-
-// for (let i = start; i <= end; i += 1) { // Change this line
-//   console.log(i);
+//   // Change code above this line
+//   return total;
 // }
+
+
+// // -------------- --------- ---- ====***** ( Exercise ) #25
+
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+  
+//    const array3 = [];
+
+// for (const element of array1) {
+//     if (array2.includes(element)) {     
+//       array3.push(element);
+//   }
+//   }
+//   return array3;
+//  // Change code above this line
+// }
+
+// // console.log(getCommonElements([1, 2, 3], [2, 4]));
+
+
+// // -------------- --------- ---- ====***** ( Exercise ) #23
+// function filterArray(numbers, value) {
+//    // Change code below this line
+  
+//   const aboveValue = [];
+
+//   // for (let i = 0; numbers[i] > value; i += 1) {
+//   for (let number of numbers) {
+//     if (number > value) {     
+//     aboveValue.push(number);
+//       continue;
+//       }
+//   }
+
+//   return aboveValue;
+
+//   // Change code above this line
+// }
+
+
+// // -------------- --------- ---- ====***** ( Exercise ) #22
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+
+//   for (let i = min; i <= max; i += 1) { // Change this line
+//     numbers.push(i);
+//   }
+  
+//   // Change code above this line
+//   return numbers;
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #21
+
+// function findLongestWord(string) {
+//   // Change code below this line
+
+//   const stringSplit = string.split(" ");
+// let longestWord = "";
+  
+//   for (const word of stringSplit) {
+//      if (word.length > longestWord.length) {
+  
+  
+// longestWord = word;
+//      }
+//   }
+  
+// return longestWord;
+//   // Change code above this line
+// }
+
+// // -------------- --------- ----====***** ( -------------- --------- ----====***** ( Exercise )  )  #20
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+//   for (let i = 0; i < order.length; i += 1) { // Change this line
+//     total += order[i];
+  
+//   }
+  
+//   // Change code above this line
+//   return total;
+// }
+
+
+// // -------------- --------- ---- ====***** ( Exercise ) #19
+
+// // const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// // for (let i = 0;  i < fruits.length; i += 1) { // Change this line
+// //   const fruit = fruits[i]; // Change this line
+// //   console.log(fruit);
+// // }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #18
+
+// function calculateTotal(number) {
+//  // Change code below this line
+
+//  let sum = 0;
+
+//   for (let i = 1; i <= number; i += 1) {
+//     sum += i;
+//   }
+
+//   return sum;
+
+//   // Change code above this line
+// }
+
+
+
+
+// // -------------- --------- ---- ====***** ( Exercise ) #17
+
+
+// // const start = 3;
+// // const end = 7;
+
+// // for (let i = start; i <= end; i += 1) { // Change this line
+// //   console.log(i);
+// // }
--- js/tests_03.js
+++ js/tests_03.js
@@ -1,55 +1,144 @@
 // "use strict";
 
-// -------------- --------- ---- ====***** ( Exercise ) #12
+// -------------- --------- ---- ====***** ( Exercise ) #18
 
-function countProps(object) {
-  let propCount = 0;
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
   // Change code below this line
+
+  const productPrice = [];
+  
+  for (const product of products) {
+    if productName = Object.product(name);
+    productPrice.push(product.price);
+}
+return productPrice;
+  // Change code above this line
+}
+
+// // -------------- --------- ---- ====***** ( Exercise ) #17
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+//  for (const color of colors) {
+//  hexColors.push(color.hex);
+//  rgbColors.push(color.rgb);
+ 
+// }
+
+
+
+// // -------------- --------- ---- ====***** ( Exercise ) #16
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+  
+//   const salary = Object.values(salaries);
+  
+//   for (const i of salary) {  
+//   totalSalary += i;  
+// }
+//   // Change code above this line
+//   return totalSalary;
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #14
+
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+
+   
+//   const keys = Object.keys(object);
+  
+//   for (const key of keys) {  
+//   propCount += 1;  
+// }
+//   return propCount;
+//   // Change code above this line
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #13
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {  
+//   values.push(apartment[key]);  
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #12
+
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
     
-    for (const prop in object) {
-        // Change code below this line
-        if (object.hasOwnProperty(prop)) {
-            propCount += 1;
-        }
-    }
-  // Change code above this line
-  return propCount;
-}
+//     for (const prop in object) {
+//         // Change code below this line
+//         if (object.hasOwnProperty(prop)) {
+//             propCount += 1;
+//         }
+//     }
+//   // Change code above this line
+//   return propCount;
+// }
 
-// -------------- --------- ---- ====***** ( Exercise ) #11
+// // -------------- --------- ---- ====***** ( Exercise ) #11
 
-const keys = [];
-const values = [];
-const advert = {
-  service: "apt",
-};
-const apartment = Object.create(advert);
-apartment.descr = "Spacious apartment in the city center";
-apartment.rating = 4;
-apartment.price = 2153;
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
 
-for (const key in apartment) {
-  // Change code below this line
- if (apartment.hasOwnProperty(key)) {
-  keys.push(key);
-  values.push(apartment[key]);
- }
-  // Change code above this line
-}
+// for (const key in apartment) {
+//   // Change code below this line
+//  if (apartment.hasOwnProperty(key)) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+//  }
+//   // Change code above this line
+// }
 
 
-// -------------- --------- ---- ====***** ( Exercise ) #10
+// // -------------- --------- ---- ====***** ( Exercise ) #10
 
-const apartment = {
-  descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-};
-const keys = [];
-const values = [];
-// Change code below this line
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
 
-for (const key in apartment) {
-    keys.push(key);
-    values.push(apartment[key]);  
-}
+// for (const key in apartment) {
+//     keys.push(key);
+//     values.push(apartment[key]);  
+// }
