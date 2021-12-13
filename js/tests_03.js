42ee10cd1ee11483fd25cddc11eaba3f1063a411
Alex <41620816+PhazZzyo@users.noreply.github.com>
2021-12-13 16:49:03 +0200

    save

--- js/tests_03.js
+++ js/tests_03.js
@@ -1,5 +1,135 @@
 // "use strict";
 
+// // -------------- --------- ---- ====***** ( Exercise ) #32
+
+// // Change code below this line
+// function addOverNum(firstNumber, ...args) {
+//   let total = 0;
+
+//   for (const arg of args) {
+//     if (firstNumber < arg) {
+//     total += arg;
+//     }
+//   }
+
+//   return total;
+//   // Change code above this line
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #31
+
+// Change code below this line
+// function add(...args) {
+//     let total = 0;
+//     for (arg of args) {
+//         total += arg;
+// }
+//   return total;
+//   // Change code above this line
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #30
+
+// function makeTask(data) {
+//   const completed = false;
+//   const category = "General";
+//   const priority = "Normal";
+//   // Change code below this line
+
+//   const newTask = { completed: false, category: "General", priority: "Normal", ...data };
+  
+//   return newTask;
+  
+//   // Change code above this line
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #29
+
+// const defaultSettings = {
+//   theme: "light",
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Change code below this line
+// const finalSettings = {...defaultSettings, ...overrideSettings};
+
+// // -------------- --------- ---- ====***** ( Exercise ) #28
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
+
+// // -------------- --------- ---- ====***** ( Exercise ) #27
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+
+// // -------------- --------- ---- ====***** ( Exercise ) #26
+
+// // Change code below this line
+// function calculateMeanTemperature(forecast) {
+  
+//   const {
+//   today: { low: todayLow, high: todayHigh },
+//   tomorrow: { low: tomorrowLow, high: tomorrowHigh }
+// } = forecast;
+
+//   // Change code above this line
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #24
+
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// const {
+//   today: { high: highToday, low: lowToday, icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" },
+//   tomorrow: { high: highTomorrow, low: lowTomorrow, icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" }
+  
+// } = forecast;
+
+// // -------------- --------- ---- ====***** ( Exercise ) #24
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { hex, rgb } of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
 // // -------------- --------- ---- ====***** ( Exercise ) #20
 
 // const products = [
