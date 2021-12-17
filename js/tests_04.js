2051f8e72c21980590f80a3cb420e333ce3b13e3
Alex <41620816+PhazZzyo@users.noreply.github.com>
2021-12-17 15:43:12 +0200

    save
    4 task

--- js/tests_04.js
+++ js/tests_04.js
@@ -1,4 +1,63 @@
+// // -------------- --------- ---- ====***** ( Exercise ) #04 /////////////////////////////////////////////
 
+const pizzaPalace = {
+  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+    
+    order(pizzaName, onSuccess, onError) {
+  if (!this.pizzas.includes(pizzaName)) {
+  return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+            }
+  return onSuccess(pizzaName);
+},
+};
+
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+pizzaPalace.order('Smoked', makePizza, onOrderError);
+pizzaPalace.order('Four meats', makePizza, onOrderError);
+pizzaPalace.order('Big Mike', makePizza, onOrderError);
+pizzaPalace.order('Vienna', makePizza, onOrderError);
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   // Имитируем доступеность абонента случайным числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+
+//   onAvailable(recipient);
+// }
+
+// function takeCall(name) {
+//   console.log(`Соединяем с ${name}, ожидайте...`);
+//   // Логика принятия звонка
+// }
+
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
+//   // Логика активации автоответчика
+// }
+
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступен, записываем голограмму.`);
+//   // Логика записи голограммы
+// }
+
+// processCall("Mango", takeCall, activateAnsweringMachine);
+// processCall("Poly", takeCall, leaveHoloMessage);
 
 // // -------------- --------- ---- ====***** ( Exercise ) #03 /////////////////////////////////////////////
 
