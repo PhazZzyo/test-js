c65fdc09d6c4681df8a72d02f60e48c350ca3d56
Alex <41620816+PhazZzyo@users.noreply.github.com>
2021-12-19 21:19:01 +0200

    save

--- js/tests_04.js
+++ js/tests_04.js
@@ -1,18 +1,287 @@
-// // -------------- --------- ---- ====***** ( Exercise ) #11 /////////////////////////////////////////////
+// // -------------- --------- ---- ====***** ( Exercise ) #25 /////////////////////////////////////////////
 
 // Change code below this line
-const getCommonElements = (firstArray, secondArray) => {
-  const commonElements = [];
+const getFriends = (users) => {
+const allFriends = users.flatMap(user => user.friends);
+const uniqueFriends = allFriends.filter(
+  (friends, index, array) => array.indexOf(friends) === index
+ );
+  return uniqueFriends;
+};
+// Change code above this line
 
-  firstArray.forEach((element) => {
-    if (secondArray.includes(element)) {
-      commonElements.push(element);
-    }
-  });
+// // -------------- --------- ---- ====***** ( Exercise ) #24 /////////////////////////////////////////////
 
-  // Change code above this line
-  return commonElements;
-}
+// // Change code below this line
+// const getUsersWithFriend = (users, friendName) => {
+//    const usersFriendArray = users.filter((user) => user.friends.includes(friendName)); 
+//   return usersFriendArray;
+// };
+// // Change code above this line
+
+// // -------------- --------- ---- ====***** ( Exercise ) #23 /////////////////////////////////////////////
+
+// // Change code below this line
+// const getUsersWithAge = (users, minAge, maxAge) => {
+ 
+//   const usersAgeArray = users.filter((user) => user.age >= minAge && user.age <= maxAge); 
+//   return usersAgeArray;
+
+// };
+// // Change code above this line
+
+// // -------------- --------- ---- ====***** ( Exercise ) #22 /////////////////////////////////////////////
+
+// // Change code below this line
+// const getUsersWithEyeColor = (users, color) => {
+
+// const userName = users.filter((user) => user.eyeColor === color); 
+//   return userName;
+
+// };
+// // Change code above this line
+
+// // -------------- --------- ---- ====***** ( Exercise ) #21 /////////////////////////////////////////////
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+// // Change code below this line
+
+// const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
+// const booksByAuthor = books.filter(book => book.author === AUTHOR);
+
+// // -------------- --------- ---- ====***** ( Exercise ) #20 /////////////////////////////////////////////
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(book => book.genres);
+// const uniqueGenres = allGenres.filter(
+//   (genres, index, array) => array.indexOf(genres) === index
+// );
+
+// // -------------- --------- ---- ====***** ( Exercise ) #19 /////////////////////////////////////////////
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(value => value % 2 === 0);
+// const oddNumbers = numbers.filter(value => value % 2 != 0);
+
+// // -------------- --------- ---- ====***** ( Exercise ) #17 /////////////////////////////////////////////
+
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ["ipsum", "lorem"],
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ["adipisicing", "irure", "velit"],
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ["ex", "culpa", "nostrud"],
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ["non", "amet", "ipsum"],
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ["lorem", "veniam", "culpa"],
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
+
+// // Change code below this line
+// const getUserNames = users => {
+
+//   return names = users.map(user => user.name);
+
+// };
+  
+//   // Change code above this line
+
+// // -------------- --------- ---- ====***** ( Exercise ) #16 /////////////////////////////////////////////
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism"],
+//   },
+// ];
+// // Change code below this line
+
+// const genres = books.flatMap(book => book.genres);
+
+// // -------------- --------- ---- ====***** ( Exercise ) #15 /////////////////////////////////////////////
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const titles = books.map(book => book.title);
+
+// // -------------- --------- ---- ====***** ( Exercise ) #14 /////////////////////////////////////////////
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+// const planetsLengths = planets.map((planet) => planet.length);
+// console.log(planetsLengths);
+
+
+// // -------------- --------- ---- ====***** ( Exercise ) #13 /////////////////////////////////////////////
+
+// function changeEven(numbers, value) {
+//   // Change code below this line
+//   const newArray = [];  
+//   numbers.forEach(element => {
+//       if (element % 2 === 0) {
+//       newArray.push(element + value);
+//     } else { 
+//       newArray.push(element);
+//     };
+    
+//   });
+// return newArray;
+//   // Change code above this line
+// }
+
+// // -------------- --------- ---- ====***** ( Exercise ) #11 /////////////////////////////////////////////
+
+// // Change code below this line
+// const getCommonElements = (firstArray, secondArray) => {
+//   const commonElements = [];
+
+//   firstArray.forEach((element) => {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+
+//   // Change code above this line
+//   return commonElements;
+// }
 
 // // -------------- --------- ---- ====***** ( Exercise ) #11 /////////////////////////////////////////////
 
