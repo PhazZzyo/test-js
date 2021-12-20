ee76bcfa9aed6d83921ae07db01a708d6fec90f5
Alex <41620816+PhazZzyo@users.noreply.github.com>
2021-12-20 15:46:39 +0200

    Update tests_04.js

--- js/tests_04.js
+++ js/tests_04.js
@@ -1,13 +1,50 @@
+
+// // -------------- --------- ---- ====***** ( Exercise ) #28 /////////////////////////////////////////////
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+// ];
+// const BOOK_TITLE = 'The Dream of a Ridiculous Man';
+// const AUTHOR = 'Robert Sheckley';
+// // Change code below this line
+
+// const bookWithTitle = books.find((book) => book.title === BOOK_TITLE);
+// const bookByAuthor = books.find((book) => book.author === AUTHOR);
+
+// // -------------- --------- ---- ====***** ( Exercise ) #25 /////////////////////////////////////////////
+
+// // Change code below this line
+// const getActiveUsers = (users, isActive) => {
+//    const activeUsers = users.filter((user) => user.isActive === true); 
+//   return activeUsers;
+// };
+// // Change code above this line
+
 // // -------------- --------- ---- ====***** ( Exercise ) #25 /////////////////////////////////////////////
 
 // Change code below this line
-const getFriends = (users) => {
-const allFriends = users.flatMap(user => user.friends);
-const uniqueFriends = allFriends.filter(
-  (friends, index, array) => array.indexOf(friends) === index
- );
-  return uniqueFriends;
-};
+// const getFriends = (users) => {
+// const allFriends = users.flatMap(user => user.friends);
+// const uniqueFriends = allFriends.filter(
+//   (friends, index, array) => array.indexOf(friends) === index
+//  );
+//   return uniqueFriends;
+// };
 // Change code above this line
 
 // // -------------- --------- ---- ====***** ( Exercise ) #24 /////////////////////////////////////////////
