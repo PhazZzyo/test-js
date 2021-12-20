63d38047608e2d2cbff18a60cbaf1a2814165f8b
Alex <41620816+PhazZzyo@users.noreply.github.com>
2021-12-20 16:53:47 +0200

    Update tests_04.js

--- js/tests_04.js
+++ js/tests_04.js
@@ -1,3 +1,37 @@
+// // -------------- --------- ---- ====***** ( Exercise ) #34 /////////////////////////////////////////////
+
+// const players = {
+//   mango: 1270,
+//   poly: 468,
+//   ajax: 710,
+//   kiwi: 244
+// };
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// // Change code below this line
+
+// const totalPlayTime = playtimes.reduce((total, values) => {
+//   return total + values;
+// }, 0);
+
+
+// // Change code above this line
+// const averagePlayTime = totalPlayTime / playtimes.length;
+
+
+////////////// example /////////////////
+
+// массив.reduce((previousValue, element, index, array) => {
+//   // Тело коллбек-функции
+// }, initialValue);
+
+////----------------
+
+// const total = [2, 7, 3, 14, 6].reduce((previousValue, number) => {
+//   return previousValue + number;
+// }, 0);
+
+// console.log(total); // 32
+
 // // -------------- --------- ---- ====***** ( Exercise ) #32 /////////////////////////////////////////////
 
 // // Change code below this line
