ea2a7fd58317dfe2a02e3fd5b95bfd32225af10a
Alex <41620816+PhazZzyo@users.noreply.github.com>
2021-12-20 16:17:11 +0200

    Update tests_04.js

--- js/tests_04.js
+++ js/tests_04.js
@@ -1,3 +1,36 @@
+// // -------------- --------- ---- ====***** ( Exercise ) #32 /////////////////////////////////////////////
+
+// // Change code below this line
+// const isEveryUserActive = (users) => {
+//    const everyUserIsActive = users.every((user) => user.isActive === true);
+//   return everyUserIsActive;
+// };
+// // Change code above this line
+
+// // -------------- --------- ---- ====***** ( Exercise ) #30 /////////////////////////////////////////////
+
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change code below this line
+
+// const eachElementInFirstIsEven = firstArray.every((value) => value %2 === 0);
+// const eachElementInFirstIsOdd = firstArray.every((value) => value %2 != 0);
+
+// const eachElementInSecondIsEven = secondArray.every((value) => value %2 === 0);
+// const eachElementInSecondIsOdd = secondArray.every((value) => value %2 != 0);
+
+// const eachElementInThirdIsEven = thirdArray.every((value) => value %2 === 0);
+// const eachElementInThirdIsOdd = thirdArray.every((value) => value %2 != 0);
+
+// // -------------- --------- ---- ====***** ( Exercise ) #29 /////////////////////////////////////////////
+
+// // Change code below this line
+// const getUserWithEmail = (users, email) => {
+// const userName = users.find((user) => user.email === email);
+//   return userName;
+// };
+// // Change code above this line
 
 // // -------------- --------- ---- ====***** ( Exercise ) #28 /////////////////////////////////////////////
 
