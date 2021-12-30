5a2a6e894116b399de3a084e332f2c52507d7a18
Alex <41620816+PhazZzyo@users.noreply.github.com>
2021-12-30 16:29:45 +0200

    Update tests_05.js

--- js/tests_05.js
+++ js/tests_05.js
@@ -1,3 +1,24 @@
+class Car {
+    // Change code below this line
+    #brand;
+
+    constructor({ brand, model, price }) {
+        this.#brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    getBrand() {
+        return this.#brand;
+    }
+
+    changeBrand(newBrand) {
+        this.#brand = newBrand;
+    }
+
+    // Change code above this line
+}
+
 // // -------------- --------- ---- ====***** ( Exercise ) #11 /////////////////////////////////////////////
 
 // class StringBuilder {
